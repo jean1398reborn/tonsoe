5e19cd9621b99181371c5c223cde50149cd0cca3
Jean Du Plessis <46666120+jean1398reborn@users.noreply.github.com>
2022-08-25 23:06:32 +1000

    Basic Bot Struct w/Token

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 //! ### What is Tonsoe?
 //! Tonsoe is an open-source discord library created for and in Rust, with a focus on bot development.
 //! The library is currently in an extremely experimental state where changes are made rapidly and will be breaking.
+
+pub mod bot_client;
