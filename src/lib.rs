b2f19a73ded22c16097d19ae4badbc69c1fe35fd
Jean Du Plessis <46666120+jean1398reborn@users.noreply.github.com>
2022-08-25 22:24:35 +1000

    Add Basic docstring to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+//! ### What is Tonsoe?
+//! Tonsoe is an open-source discord library created for and in Rust, with a focus on bot development.
+//! The library is currently in an extremely experimental state where changes are made rapidly and will be breaking.
