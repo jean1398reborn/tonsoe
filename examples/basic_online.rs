7899d0e56620244a502008fe1cec6096332273c1
Jean Du Plessis <46666120+jean1398reborn@users.noreply.github.com>
2022-08-25 23:11:24 +1000

    Add basic BotBuilder to example

--- examples/basic_online.rs
+++ examples/basic_online.rs
@@ -1,5 +1,16 @@
 //! Example showing most basic bot functionality of coming online.
- 
+// Not Complete!
+
+use std::env;
+use tonsoe::bot_client::*;
+
 fn main() {
-    println!("Hello World!");
+
+    // Grab the token from https://discord.com/developers
+    // Note: You need to get this before the [`BotBuilder`] is initialised as it cannot be changed after.
+    let token: String = String::from("Token");
+
+    //Create a new Builder for our [`Bot`] using [`BotBuilder`]
+    let bot = BotBuilder::new(token);
+
 }
