ea7cb437e64462a0c9fb763db80d52ac56496e94
Jean Du Plessis <46666120+jean1398reborn@users.noreply.github.com>
2022-08-26 08:29:51 +1000

    Change Comments to Doc Comments

--- src/bot_client.rs
+++ src/bot_client.rs
@@ -1,22 +1,22 @@
 
-// Basic structure which represents a Bot inside the library
-// Not the same as a discord bot!
+/// Basic structure which represents a Bot inside the library
+/// Not the same as a discord bot!
 pub struct Bot {
 
-    // The discord token for the bot utilised for connecting & accessing the discord api.
+    /// The discord token for the bot utilised for connecting & accessing the discord api.
     pub token: String,
 }
 
-// A Builder for [`Bot`]
-// 
-// Utilises the builder pattern to help ease users in creating a [`Bot`] due to the complexity of the type.
+/// A Builder for [`Bot`]
+/// 
+/// Utilises the builder pattern to help ease users in creating a [`Bot`] due to the complexity of the type.
 pub struct BotBuilder {
     pub token: String
 }
 
 impl BotBuilder {
-    // Creates a new [`BotBuilder`] with the basic required fields as args
-    // The rest can be set utilising the methods on this builder or are created when this builder is built.
+    /// Creates a new [`BotBuilder`] with the basic required fields as args
+    /// The rest can be set utilising the methods on this builder or are created when this builder is built.
     pub fn new(token: String) -> Self {
         Self {
             token,
